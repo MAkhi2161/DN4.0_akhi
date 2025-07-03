764e8cb3d28db43010ef2498d89915c079f7a7ff
Akhila Marisetty <150886952+MAkhi2161@users.noreply.github.com>
2025-07-03 11:39:06 +0530

    Added code for OrmLearnApplication.java
    Initialized Spring Boot application and added logic to retrieve a country by its code using CountryRepository.  
    Displays country name on successful fetch, or a not found message otherwise.

---
+++ week3/Spring-data-jpa/Exercise1/OrmLearnApplication.java
@@ -0,0 +1,30 @@
+package com.cognizant.ormlearn;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmLearnApplication implements CommandLineRunner {
+
+	@Autowired
+	private CountryRepository countryRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmLearnApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Add logic here
+		Country country = countryRepository.findById("IN").orElse(null);
+		if (country != null) {
+			System.out.println("Country Name: " + country.getName());
+		} else {
+			System.out.println("Country with code IN not found.");
+		}
+	}
+}
