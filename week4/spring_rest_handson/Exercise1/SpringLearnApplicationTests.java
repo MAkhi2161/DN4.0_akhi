31729ddfd9f6ae7e0149dad7aba0a8e515bc2b7c
Akhila Marisetty <150886952+MAkhi2161@users.noreply.github.com>
2025-07-07 09:17:34 +0530

    Added SpringLearnApplicationTests.java
    This is your test class that verifies Spring context loads.

---
+++ week4/spring_rest_handson/Exercise1/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.spring_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test just checks if the Spring context loads without any issues
+	}
+}
