092a0dcc6d8189121964d28e369788b9f9cb5233
Akhila Marisetty <150886952+MAkhi2161@users.noreply.github.com>
2025-07-28 09:45:34 +0530

    Added BlogDetails.js

---
+++ week7/React/HandsOn13/src/components/BlogDetails.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function BlogDetails() {
+  return (
+    <div className="box">
+      <h2>Blog Details</h2>
+      <p><strong>React Learning</strong><br />
+        <em>Stephen Biz</em><br />
+        Welcome to learning React!
+      </p>
+      <p><strong>Installation</strong><br />
+        <em>Schwezdenier</em><br />
+        You can install React from npm.
+      </p>
+    </div>
+  );
+}
+
+export default BlogDetails;
