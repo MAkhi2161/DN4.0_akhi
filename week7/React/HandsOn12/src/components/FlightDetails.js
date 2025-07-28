69ffc9a963cee17621830b8546d2132162de0f33
Akhila Marisetty <150886952+MAkhi2161@users.noreply.github.com>
2025-07-28 09:22:26 +0530

    Added FlightDetails.js

---
+++ week7/React/HandsOn12/src/components/FlightDetails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function FlightDetails() {
+  return (
+    <div>
+      <h3>Available Flights</h3>
+      <ul>
+        <li>Flight 1: Hyderabad to Delhi</li>
+        <li>Flight 2: Mumbai to Bangalore</li>
+        <li>Flight 3: Chennai to Kolkata</li>
+      </ul>
+    </div>
+  );
+}
+
+export default FlightDetails;
