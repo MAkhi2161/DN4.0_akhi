66cbc50541d3bd9b1e7829a069dba6aaa954ee2a
Akhila Marisetty <150886952+MAkhi2161@users.noreply.github.com>
2025-07-28 09:46:20 +0530

    Added CourseDetails.js

---
+++ week7/React/HandsOn13/src/components/CourseDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CourseDetails() {
+  return (
+    <div className="section">
+      <h2>Course Details</h2>
+      <p><strong>Angular</strong><br />4/5/2025</p>
+      <p><strong>React</strong><br />6/3/2025</p>
+    </div>
+  );
+}
+
+export default CourseDetails;
